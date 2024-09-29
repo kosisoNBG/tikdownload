2f0f3764218f4d75c209a3629c8753888e6625e8
yelnats20 <itisemzy@gmail.com>
2024-09-29 16:23:38 +0100

    Refractored The Project

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer/Footer';
-// import Socials from './components/Socials/Socials';
 import VideoDownload from './components/VideoDownload/VideoDownload';
 import Faq from './components/Faq';
 import Main from './components/Contents/Main';
 
 function App() {
-  
   return (
-    <div className="App row">
+    <div className="App">
       <Header />
-      <header className="App-header jumbotron">
-        <h1>Tiktok Video Downloader</h1>
-        <h3>Download and Save TikTok Videos Without Watermark.</h3>
-      <VideoDownload />
-      </header>
+      <div className="container bg-primary text-white" style={{ padding: '20px', borderRadius: '5px' }}>
+        <h1 className="text-center">Tiktok Video Downloader</h1>
+        <h3 className="text-center">Download and Save TikTok Videos Without Watermark.</h3>
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <VideoDownload />
+          </div>
+        </div>
+      </div>
       <Main />
       <Faq />
-      {/* <Socials /> */}
       <Footer />
     </div>
   );
